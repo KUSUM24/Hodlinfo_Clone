0e0af9327cebe8c1b6a2e352a4853569b0cf95b5
KUSUM24 <2019kusum.rohra@ves.ac.in>
2021-05-15 02:15:38 +0530

    scroll removed

--- src/components/Footers/Fixed.jsx
+++ src/components/Footers/Fixed.jsx
@@ -2,7 +2,10 @@ import React from "react";
 
 export const Fixed = () => {
   return (
-    <footer className="bg-dark" style={{ position: "sticky", bottom: "0px" }}>
+    <footer
+      className="bg-dark"
+      style={{ position: "sticky", bottom: "0px", fontFamily: "sans-serif" }}
+    >
       <div className="btn btn-outline-info m-1">
         {" "}
         Add holinfo to home screen
--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -6,11 +6,17 @@ import { Table } from "react-bootstrap";
 export const Main = () => {
   let width = window.innerWidth;
   return (
-    <div className="bg-dark">
-      <FirstImage width={width} />
-      <Table responsive hover variant="dark" style={{ fontSize: "100%" }}>
+    <div className="bg-dark" style={{ fontFamily: "sans-serif" }}>
+      <FirstImage width={width - 20} />
+      <Table
+        responsive
+        striped
+        hover
+        variant="dark"
+        style={{ fontSize: "100%" }}
+      >
         <thead>
-          <tr style={{ maxHeight: "20px" }}>
+          <tr style={{ height: "20px", maxHeight: "20px" }}>
             <th>#</th>
             <th>Platform</th>
             <th>Last Traded Price</th>
@@ -25,52 +31,52 @@ export const Main = () => {
             <td>WazirX</td>
             <td>₹ 40,49,595</td>
             <td>₹ 40,49,439 / ₹ 40,49,595</td>
-            <td>6.04 %</td>
-            <td>▲ ₹ 2,30,623</td>
+            <td className="text-info">6.04 %</td>
+            <td className="text-info">▲ ₹ 2,30,623</td>
           </tr>
           <tr>
             <td>2</td>
             <td>Bitbns</td>
             <td>₹ 40,49,595</td>
             <td>₹ 40,49,439 / ₹ 40,49,595</td>
-            <td>6.04 %</td>
-            <td>▲ ₹ 2,30,623</td>
+            <td className="text-info">6.04 %</td>
+            <td className="text-info">▲ ₹ 2,30,623</td>
           </tr>
           <tr>
             <td>3</td>
             <td>Giotus</td>
             <td>₹ 40,49,595</td>
             <td>₹ 40,49,439 / ₹ 40,49,595</td>
-            <td>6.04 %</td>
-            <td>▲ ₹ 2,30,623</td>
+            <td className="text-info">6.04 %</td>
+            <td className="text-info">▲ ₹ 2,30,623</td>
           </tr>
           <tr>
             <td>4</td>
             <td>Colodax</td>
             <td>₹ 40,49,595</td>
             <td>₹ 25,75,167 / ₹ 26,08,984</td>
-            <td>6.04 %</td>
-            <td>▲ ₹ 2,30,623</td>
+            <td className="text-info">6.04 %</td>
+            <td className="text-info">▲ ₹ 2,30,623</td>
           </tr>
           <tr>
             <td>5</td>
             <td>Zebpay</td>
             <td>₹ 40,49,595</td>
             <td>₹ 40,49,439 / ₹ 40,49,595</td>
-            <td>6.04 %</td>
-            <td>▲ ₹ 2,30,623</td>
+            <td className="text-info">6.04 %</td>
+            <td className="text-info">▲ ₹ 2,30,623</td>
           </tr>
           <tr>
             <td>6</td>
             <td>CoinDCX</td>
             <td>₹ 40,49,595</td>
             <td>₹ 40,49,439 / ₹ 40,49,595</td>
-            <td>6.04 %</td>
-            <td>▲ ₹ 2,30,623</td>
+            <td className="text-info">6.04 %</td>
+            <td className="text-info">▲ ₹ 2,30,623</td>
           </tr>
         </tbody>
       </Table>
-      <SecondImage width={width} />
+      <SecondImage width={width - 20} />
     </div>
   );
 };
