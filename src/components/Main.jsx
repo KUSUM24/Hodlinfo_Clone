149c353fd9c061cffc5d9b20d5f8ab6c591e474b
KUSUM24 <2019kusum.rohra@ves.ac.in>
2021-06-18 21:44:01 +0530

    New Table

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -8,74 +8,70 @@ export const Main = () => {
   return (
     <div className="bg-dark" style={{ fontFamily: "sans-serif" }}>
       <FirstImage width={width - 20} />
-      <Table
-        responsive
-        striped
-        hover
-        variant="dark"
-        style={{ fontSize: "100%" }}
-      >
-        <thead>
-          <tr style={{ height: "20px", maxHeight: "20px" }}>
-            <th>#</th>
-            <th>Platform</th>
-            <th>Last Traded Price</th>
-            <th>Buy / Sell Price</th>
-            <th>Difference</th>
-            <th>Savings</th>
-          </tr>
-        </thead>
-        <tbody>
-          <tr>
-            <td>1</td>
-            <td>WazirX</td>
-            <td>₹ 40,49,595</td>
-            <td>₹ 40,49,439 / ₹ 40,49,595</td>
-            <td className="text-info">6.04 %</td>
-            <td className="text-info">▲ ₹ 2,30,623</td>
-          </tr>
-          <tr>
-            <td>2</td>
-            <td>Bitbns</td>
-            <td>₹ 40,49,595</td>
-            <td>₹ 40,49,439 / ₹ 40,49,595</td>
-            <td className="text-info">6.04 %</td>
-            <td className="text-info">▲ ₹ 2,30,623</td>
-          </tr>
-          <tr>
-            <td>3</td>
-            <td>Giotus</td>
-            <td>₹ 40,49,595</td>
-            <td>₹ 40,49,439 / ₹ 40,49,595</td>
-            <td className="text-info">6.04 %</td>
-            <td className="text-info">▲ ₹ 2,30,623</td>
-          </tr>
-          <tr>
-            <td>4</td>
-            <td>Colodax</td>
-            <td>₹ 40,49,595</td>
-            <td>₹ 25,75,167 / ₹ 26,08,984</td>
-            <td className="text-info">6.04 %</td>
-            <td className="text-info">▲ ₹ 2,30,623</td>
-          </tr>
-          <tr>
-            <td>5</td>
-            <td>Zebpay</td>
-            <td>₹ 40,49,595</td>
-            <td>₹ 40,49,439 / ₹ 40,49,595</td>
-            <td className="text-info">6.04 %</td>
-            <td className="text-info">▲ ₹ 2,30,623</td>
-          </tr>
-          <tr>
-            <td>6</td>
-            <td>CoinDCX</td>
-            <td>₹ 40,49,595</td>
-            <td>₹ 40,49,439 / ₹ 40,49,595</td>
-            <td className="text-info">6.04 %</td>
-            <td className="text-info">▲ ₹ 2,30,623</td>
-          </tr>
-        </tbody>
-      </Table>
+      <div className="table-responsive">
+        <table className="table text-white">
+          <thead>
+            <tr>
+              <th scope="col">#</th>
+              <th scope="col">Platform</th>
+              <th scope="col">Last Traded Price</th>
+              <th scope="col">Buy / Sell Price</th>
+              <th scope="col">Difference</th>
+              <th scope="col">Savings</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr>
+              <td scope="row">1</td>
+              <td>WazirX</td>
+              <td>₹ 40,49,595</td>
+              <td>₹ 40,49,439 / ₹ 40,49,595</td>
+              <td className="text-info">6.04 %</td>
+              <td className="text-info">▲ ₹ 2,30,623</td>
+            </tr>
+            <tr>
+              <td scope="row">2</td>
+              <td>Bitbns</td>
+              <td>₹ 40,49,595</td>
+              <td>₹ 40,49,439 / ₹ 40,49,595</td>
+              <td className="text-info">6.04 %</td>
+              <td className="text-info">▲ ₹ 2,30,623</td>
+            </tr>
+            <tr>
+              <td scope="row">3</td>
+              <td>Giotus</td>
+              <td>₹ 40,49,595</td>
+              <td>₹ 40,49,439 / ₹ 40,49,595</td>
+              <td className="text-info">6.04 %</td>
+              <td className="text-info">▲ ₹ 2,30,623</td>
+            </tr>
+            <tr>
+              <td scope="row">4</td>
+              <td>Colodax</td>
+              <td>₹ 40,49,595</td>
+              <td>₹ 25,75,167 / ₹ 26,08,984</td>
+              <td className="text-info">6.04 %</td>
+              <td className="text-info">▲ ₹ 2,30,623</td>
+            </tr>
+            <tr>
+              <td scope="row">5</td>
+              <td>Zebpay</td>
+              <td>₹ 40,49,595</td>
+              <td>₹ 40,49,439 / ₹ 40,49,595</td>
+              <td className="text-info">6.04 %</td>
+              <td className="text-info">▲ ₹ 2,30,623</td>
+            </tr>
+            <tr>
+              <td scope="row">6</td>
+              <td>CoinDCX</td>
+              <td>₹ 40,49,595</td>
+              <td>₹ 40,49,439 / ₹ 40,49,595</td>
+              <td className="text-info">6.04 %</td>
+              <td className="text-info">▲ ₹ 2,30,623</td>
+            </tr>
+          </tbody>
+        </table>
+      </div>
       <SecondImage width={width - 20} />
     </div>
   );
